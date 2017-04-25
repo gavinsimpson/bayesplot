fd97d0050c458fffaeefcd2a8282d5cfcce7bd1a
Gavin Simpson <ucfagls@gmail.com>
2017-04-25 08:28:58 -0600

    set default theme when attaching package

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onAttach <- function(...) {
   ver <- utils::packageVersion("bayesplot")
+  ## set the default theme
+  ggplot2::theme_set(theme_default())
   packageStartupMessage("This is bayesplot version ", ver)
 }
